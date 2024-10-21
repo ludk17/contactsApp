81d48cb83680387c28e988b9a96d55511a12b0b9
PaulRodasM <paulrodasmalca@gmail.com>
2024-10-21 12:17:55 -0500

    SEM 11-1 PAUL

--- app/src/main/java/com/upn/contactsapp/activities/FirebaseActivity.java
+++ app/src/main/java/com/upn/contactsapp/activities/FirebaseActivity.java
@@ -24,12 +24,12 @@ public class FirebaseActivity extends AppCompatActivity {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference myRef = database.getReference("contacts");
 
-            Contact c1 = new Contact("Juan","123456");
-            c1.id=1;
+            Contact ct1 = new Contact("Juan","123454556");
+            ct1.id=1;
             Contact c2 = new Contact("Pedro","1255534576");
             c2.id=2;
 
-            myRef.child(String .valueOf(c1.id)).setValue(c1);
+            myRef.child(String .valueOf(ct1.id)).setValue(ct1);
             myRef.child(String .valueOf(c2.id)).setValue(c2);
         });
 
