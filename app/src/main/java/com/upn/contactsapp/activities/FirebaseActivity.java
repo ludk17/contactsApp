e151a3b5960a2be1a170f4d6226278866c5f6e61
PaulRodasM <paulrodasmalca@gmail.com>
2024-10-21 09:04:21 -0500

    USO DE FIRE BASE

---
+++ app/src/main/java/com/upn/contactsapp/activities/FirebaseActivity.java
@@ -0,0 +1,37 @@
+package com.upn.contactsapp.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.upn.contactsapp.R;
+import com.upn.contactsapp.entities.Contact;
+
+public class FirebaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+
+        Button btn = findViewById(R.id.btnCreateOnFirebase);
+        btn.setOnClickListener(v -> {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("contacts");
+
+            Contact c1 = new Contact("Juan","123456");
+            c1.id=1;
+            Contact c2 = new Contact("Carlos","123456");
+            c2.id=2;
+
+            myRef.child(String .valueOf(c1.id)).setValue(c1);
+            myRef.child(String .valueOf(c2.id)).setValue(c2);
+        });
+
+    }
+}
