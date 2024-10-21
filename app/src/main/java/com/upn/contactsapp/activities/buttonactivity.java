b67497a44aa90132b5974a289810371c8ddeed7c
ErickJosue322 <erick12345678910191@gmail.com>
2024-10-21 08:58:31 -0500

    actualizaciones semana 11

---
+++ app/src/main/java/com/upn/contactsapp/activities/buttonactivity.java
@@ -0,0 +1,29 @@
+package com.upn.contactsapp.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.upn.contactsapp.R;
+import com.upn.contactsapp.entities.Contact;
+
+public class FireBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_firebase);
+
+buton boton = find
+    }
+}
